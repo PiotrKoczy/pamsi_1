2b5caeaa3dc3640dbcb2f14481e94585037169be
PiotrKoczy <259367@student.pwr.edu.pl>
2022-03-31 03:38:05 +0200

    quick sort raczej działa

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -36,41 +36,69 @@ int middleOfThree(int a, int b, int c)
         return c;
 }
 
-int generatePivotValue(int array[], int leftIDX, int rightIDX){
-    int mid = leftIDX+(rightIDX-leftIDX)/2;
-    swap(array[mid],array[rightIDX]); //14:50 po co??
+int generatePivotValue(int array[], int leftIDX, int rightIDX)
+{
+    int mid = leftIDX + (rightIDX - leftIDX) / 2;
     return array[mid];
 }
 
-void quickSort(int array[], int leftIDX, int rightIDX)
+// void quickSort(int array[], int leftIDX, int rightIDX)
+// {
+//     int extraArray[SIZE];
+
+//     int finger = leftIDX;
+//     int border = leftIDX - 1;
+
+//     int pivot = generatePivotValue(array,leftIDX,rightIDX);
+//     //int pivot = array[rightIDX];
+//     if (leftIDX < rightIDX)
+//     {
+//         while (finger < rightIDX)
+//         {
+//             if (array[finger] < pivot)
+//             {
+//                 border++;
+//                 if (border != finger)
+//                     swap(array[finger], array[border]);
+//             }
+//             finger++;
+//             printArray(array, SIZE);
+//         }
+//         border++;
+//         if (border != rightIDX)
+//         {
+//             swap(array[rightIDX], array[border]);
+//         }
+//         quickSort(array, leftIDX, border - 1);
+//         quickSort(array, border + 1, rightIDX);
+//     }
+// }
+
+void quickSort(int array[], int size)
 {
-    int extraArray[SIZE];
-
-    int finger = leftIDX;
-    int border = leftIDX - 1;
-
-    int pivot = generatePivotValue(array,leftIDX,rightIDX);
-    //int pivot = array[rightIDX];
+    int leftIDX = 0;
+    int rightIDX = size - 1;
+    printArray(array, size);
     if (leftIDX < rightIDX)
     {
-        while (finger < rightIDX)
+        int pivotValue = generatePivotValue(array, leftIDX, rightIDX);
+        while (leftIDX <= rightIDX)
         {
-            if (array[finger] < pivot)
+            printArray(array, size);
+            while (array[leftIDX] < pivotValue)
+                leftIDX++;
+            while (array[rightIDX] > pivotValue)
+                rightIDX--;
+            if (leftIDX <= rightIDX) // ta nierówność jest spełniona jeśli po obu stronach znalazła się liczba do przestawienia
             {
-                border++;
-                if (border != finger)
-                    swap(array[finger], array[border]);
+                if (array[leftIDX] != array[rightIDX])
+                    swap(array[leftIDX], array[rightIDX]);
+                leftIDX++;
+                rightIDX--;
             }
-            finger++;
-            printArray(array, SIZE);
         }
-        border++;
-        if (border != rightIDX)
-        {
-            swap(array[rightIDX], array[border]);
-        }
-        quickSort(array, leftIDX, border - 1);
-        quickSort(array, border + 1, rightIDX);
+        quickSort(array, rightIDX + 1); //od lewej krawędzi do pierwszej nieuporządowanej liczby
+        quickSort(&array[leftIDX], size - leftIDX); //od pierwszej nieuporządkowanej liczby do prawej krawędzi
     }
 }
 
@@ -87,10 +115,12 @@ int main()
     //     testArray[i] = number;
     //     testArray2[i] = number;
     // }
-    int testArray[] = {9, 1, 2, 4, 5, 7, 8, 6, 3};
+    // int testArray[] = {9, 1, 2, 4, 5, 7, 8, 6, 3};
+    // int testArray[] = {4,7,8,6,4,6,7,5,10};
+    int testArray[] = {0, 1, 2, 3, 4, 5, 7, 5, 10};
 
     printArray(testArray, size);
-    quickSort(testArray, 0, size - 1);
+    quickSort(testArray, size);
 
     cout << "Sorted array: \n";
     printArray(testArray, size);
